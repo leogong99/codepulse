3bfb1a9ba5457cbba9603aade7c73badde8f6b95
Leo Gong <leogong99@gmail.com>
2026-05-24 10:03:37 -0700

    chore: revert test

--- packages/core/src/indexer/HashUtil.ts
+++ packages/core/src/indexer/HashUtil.ts
@@ -3,4 +3,3 @@ import { createHash } from 'crypto';
 export function hashContent(content: string): string {
   return createHash('sha256').update(content).digest('hex').slice(0, 16);
 }
-export function hello() { return "world"; }
